fdd2304b0b8a6d35ac064a4a8db9af0cd122ed09
Lion <lion@luckylab.page>
2024-03-26 15:26:40 +0800

    feature: install prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,14 @@ module.exports = {
     browser: true,
     es2023: true
   },
-  extends: ['@nuxtjs/eslint-config-typescript'],
+  extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
   parserOptions: {
     ecmaVersion: 2023,
     sourceType: 'module'
   },
+  plugins: ['prettier'],
   rules: {
-    'no-undef': 'off'
+    'no-undef': 'off',
+    'prettier/prettier': 'error'
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,6 @@
   </div>
 </template>
 <script lang="ts" setup>
-const year: string = '2022'
+const year: string = '2024';
 const title: string = `${year} iThome 鐵人賽`
 </script>
