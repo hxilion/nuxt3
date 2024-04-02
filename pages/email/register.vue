fb06609f9c2189660f4c2f0206877bb39f6ac8de
Lion <lion@luckylab.page>
2024-04-02 09:57:05 +0800

    update: email page and add mobile page

--- pages/email/register.vue
+++ pages/email/register.vue
@@ -6,13 +6,13 @@
     <!-- 引入 Tailwind CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
   </head>
-  <div class="photo flex flex-col justify-center h-full px-10 pb-10 text-color-1" style="background-color: #902BF5;">
+  <div class="photo px-10 pb-10 text-color-1" style="background-color: #902BF5;width:100%">
     <div>
       <img src="/assets/img/desktop.png" alt="">
     </div>
-    <div>
+    <div style="max-width: 640px; min-width: 390px;">
       <img src="/assets/img/mail-cover.png" alt="">
-      <div class="text-sm text-left text-start font-medium p-10" style="background-color: #fff;width: 640px;">
+      <div class="text-sm text-left text-start font-medium p-10" style="background-color: #fff;">
         <div class="mb-10" style="font-family:'M PLUS 2', Sans-serif">
           %user_name% 様
         </div>
@@ -34,7 +34,7 @@
         <div class="mb-10">
           ※このURLは24時間以内に有効です。期限が過ぎた場合は、再度登録手続きを行ってください。
         </div>
-        <div class="divide mb-10">
+        <div class="divide-x mb-10">
         </div>
         <div class="photo mb-10">
           <img src="/assets/img/title-a.png" alt="">
@@ -58,7 +58,7 @@
           ※このメールに心当たりのない場合は、お手数ですがこのメールを削除してください。
         </div>
       </div>
-      <div class="text-sm text-left text-start font-medium px-10 pt-5 pb-5 rounded-b-md" style="background-color: #F4EAFE;width: 640px;">
+      <div class="text-sm text-left text-start font-medium px-10 pt-5 pb-5 rounded-b-md" style="background-color: #F4EAFE;max-width: 640px; min-width: 390px;">
         <div class="photo mb-10" style="float: right;position: relative;top: -52px;">
           <img src="/assets/img/email-ol.png" alt="">
         </div>
@@ -72,13 +72,35 @@
           ※営業時間：
         </div>
       </div>
-      <div class="text-sm text-left text-start font-medium px-10 pt-5 pb-5 rounded-b-md" style="width: 640px;">
-        <div class="flex flex-nowrap">
-          <img src="/assets/img/facebook.png" alt="">
-          <img src="/assets/img/instagram.png" alt="">
-          <img src="/assets/img/twitter.png" alt="">
-          <img src="/assets/img/youtube.png" alt="">
-          <img src="/assets/img/tiktok.png" alt="">
+      <div class="text-sm font-medium px-10 pt-5 pb-5" style="max-width: 640px; min-width: 390px;">
+        <div class="container">
+            <div class="image-wrapper">
+              <img src="/assets/img/facebook.png" alt="">
+            </div>
+            <div class="image-wrapper">
+              <img src="/assets/img/instagram.png" alt="">
+            </div>
+            <div class="image-wrapper">
+              <img src="/assets/img/twitter.png" alt="">
+            </div>
+            <div class="image-wrapper">
+              <img src="/assets/img/youtube.png" alt="">
+            </div>
+            <div class="image-wrapper">
+              <img src="/assets/img/tiktok.png" alt="">
+            </div>
+          </div>
+        <div class="photo my-10">
+          <img src="/assets/img/pazuru-logo-white.png" alt="">
+        </div>
+        <div class="font-xs" style="color: #f4eafe;">
+          <a class="divide-y" href="/">プライバシーポリシー</a>
+          <a class="divide-y" href="/">利用規約</a>
+          <a class="divide-y" href="/">よくある質問</a>
+          <a href="/">カスタマーサポート</a>
+        </div>
+        <div class="text-color-1 text-xs mt-10" style="color: #fff;">
+          ©Pazuru 2024 | 全著作権所有
         </div>
       </div>
     </div>
@@ -90,8 +112,14 @@
     text-align: -webkit-center;
   }
 
-  .divide {
+  .divide-x {
     border-bottom: 1px solid #CC9DFA;
+  }
+
+  .divide-y {
+    border-right: 1px solid #F4EAFE;
+    margin-right: 10px;
+    padding-right: 10px;
   }
 
   .text-color-1 {
@@ -101,7 +129,18 @@
     color: #727478;
   }
 
-  .social-media {
-    text-align: -webkit-right;
+  .container {
+    display: flex;
+    justify-content: center; /* 將圖片水平置中 */
+    align-items: center; /* 將圖片垂直置中 */
+  }
+
+  .image-wrapper {
+    margin: 0 10px; /* 可根據需要調整圖片之間的間距 */
+  }
+
+  .image-wrapper img {
+    max-width: 100%; /* 讓圖片保持其原始大小比例 */
+    height: auto;
   }
 </style>
