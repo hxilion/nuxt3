06108d4cf25350b5ba2dae8ab5f4c26ac089fd8b
Lion <lion@luckylab.page>
2024-04-12 13:33:53 +0800

    feture: 新增註冊完畢頁面/手機版

---
+++ pages/m/email/registerCompleted.vue
@@ -0,0 +1,260 @@
+<template>
+	<div class="photo text-color-1 font-medium" style="background-color: #902BF5;width:100%;font-family:'M PLUS 2'">
+		<div>
+			<img src="/assets/img/email/desktop.png" alt="">
+		</div>
+		<div>
+			<img src="/assets/img/email/register-completed.png" alt="">
+			<div class="text-sm text-left text-start font-medium py-10 px-5" style="background-color: #fff;">
+				<div class="mb-10">
+					%user_name% 様
+				</div>
+				<div>
+					この度はPazuruへのご登録、誠にありがとうございます。
+				</div>
+        <div class="mb-10">
+          本日より、会員限定のコンテンツやサービスがご利用いただけます。
+        </div>
+        <div class="text-center mb-5 font-bold	text-xl">
+          《 ログイン情報 》
+        </div>
+				<div class="mb-2.5">
+					<ul>
+						<li>ユーザーID：％User_ ID％</li>
+						<li>パスワード：※ご自身で設定したパスワード</li>
+					</ul>
+				</div>
+        <div class="grid-button mb-5 photo">
+          <div>
+            <a href="https://pazuru.com/ja/promotions" target="_blank">
+              <button
+                type="button"
+                class="container-flex mail-button px-3 py-5 rounded-lg text-white text-2xl font-extrabold" >ログイン
+                <img class="ml-14" src="/assets/img/email/login.png" alt="">
+              </button>
+            </a>
+          </div>
+          <div>
+            <a href="https://pazuru.com/ja/promotions" target="_blank">
+              <button
+                type="button"
+                class="container-flex mail-button px-3 py-5 rounded-lg text-white text-2xl font-extrabold" >マイページ
+                <img class="ml-14" src="/assets/img/email/my-page.png" alt="">
+              </button>
+            </a>
+          </div>
+        </div>
+        <div class="mb-10">
+					<ul>
+						<li>パスワードは、定期的に変更いただくことをおすすめします。</li>
+						<li>マイページでは、登録情報の変更や退会手続きなどが行えます。</li>
+					</ul>
+				</div>
+        <div class="text-center mb-5 font-bold	text-xl">
+          《 お問い合わせ・サポート 》
+        </div>
+        <div class="grid-button photo">
+          <div>
+            <a href="https://pazuru.com/ja/promotions" target="_blank">
+              <button
+                type="button"
+                class="container-flex mail-button p-5 rounded-lg text-white text-lg font-extrabold">
+                <div class="text-left">
+                  <div>カスタマー<</div>
+                  <div>サポートチャット</div>
+                </div>
+                <div>
+                  <img class="ml-8" src="/assets/img/email/login.png" alt="">
+                </div>
+              </button>
+            </a>
+          </div>
+          <div class="mb-14">
+            <a href="https://pazuru.com/ja/promotions" target="_blank">
+              <button
+                type="button"
+                class="container-flex mail-button px-3 py-5 rounded-lg text-white text-lg font-extrabold">
+                <div class="text-left">
+                  <div>よくある<</div>
+                  <div>お問い合わせ</div>
+                </div>
+                <div>
+                  <img class="ml-12" src="/assets/img/email/frequently-questions.png" alt="">
+                </div>
+              </button>
+            </a>
+          </div>
+        </div>
+				<div class="divide-x mb-14"></div>
+        <div class="container-flex mb-10">
+          <div class="left"></div>
+          <div class="p-2 content">
+            <span class="content-text text-2xl font-extrabold">新規登録特典</span>
+          </div>
+          <div class="right"></div> 
+        </div>
+				<div class="mb-14 text-center">
+					登録が完了したら、お得な特典をGETしよう！！
+				</div>
+        <div class="photo mb-10">
+          <img src="/assets/img/email/banner.png" alt="">
+        </div>
+				<div class="mb-14">
+					今後ともPazuruをご愛顧いただきますよう、よろしくお願いいたします。
+				</div>
+				<div class="text-color-2 text-xs">
+					※このメールは自動送信のため、返信できません。
+				</div>
+				<div class="text-color-2 text-xs">
+					※このメールに心当たりのない場合は、お手数ですがこのメールを削除してください。
+				</div>
+			</div>
+			<div class="email-conect text-sm text-left text-start px-10 pt-5 pb-5 font-medium rounded-b-md" style="background-color: #F4EAFE;max-width: 640px; min-width: 390px;">
+				<div class="email-ol">
+					<img src="/assets/img/email/email-ol.png" alt="">
+				</div>
+				<div class="text-color-1 text-xs font-bold mb-2.5">
+					Pazuru運営事務局
+				</div>
+				<div class="text-color-1 text-xs mb-2.5 whitespace-nowrap">
+					メールアドレス：info@pazuru.com
+				</div>
+				<div class="text-color-1 text-xs">
+					※営業時間：
+				</div>
+			</div>
+			<div class="email-about text-sm font-medium px-10 pt-5 pb-5" style="max-width: 640px; min-width: 390px;">
+				<div class="container-flex" style="width: 100%;">
+						<div class="image-wrapper">
+							<img src="/assets/img/email/facebook.png" alt="">
+						</div>
+						<div class="image-wrapper">
+							<img src="/assets/img/email/instagram.png" alt="">
+						</div>
+						<div class="image-wrapper">
+							<img src="/assets/img/email/twitter.png" alt="">
+						</div>
+						<div class="image-wrapper">
+							<img src="/assets/img/email/youtube.png" alt="">
+						</div>
+						<div class="image-wrapper">
+							<img src="/assets/img/email/tiktok.png" alt="">
+						</div>
+					</div>
+				<div class="my-10">
+					<img src="/assets/img/email/pazuru-logo-white.png" alt="">
+				</div>
+				<div class="text-xs" style="color: #f4eafe;">
+					<a class="divide-y" href="/">プライバシーポリシー</a>
+					<a class="divide-y" href="/">利用規約</a>
+					<a class="divide-y" href="/">よくある質問</a>
+					<a href="/">カスタマーサポート</a>
+				</div>
+				<div class="text-xs mt-10" style="color: #fff;">
+					©Pazuru 2024 | 全著作権所有
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+	.photo {
+		text-align: -webkit-center;
+	}
+
+	.divide-x {
+		border-bottom: 1px solid #CC9DFA;
+	}
+
+	.divide-y {
+		border-right: 1px solid #F4EAFE;
+		margin-right: 10px;
+		padding-right: 10px;
+	}
+
+	.text-color-1 {
+		color: #131416;
+	}
+	.text-color-2 {
+		color: #727478;
+	}
+
+	.container-flex {
+		display: flex;
+		justify-content: center; /* 將圖片水平置中 */
+		align-items: center; /* 將圖片垂直置中 */
+	}
+
+	.image-wrapper {
+		margin: 0 10px; /* 可根據需要調整圖片之間的間距 */
+	}
+
+	.image-wrapper img {
+		max-width: 100%; /* 讓圖片保持其原始大小比例 */
+		height: auto;
+	}
+
+  .mail-button {
+    width: 270px;
+    height: 100px;
+    background: linear-gradient(#a655f7, #902BF5);
+    border: 2px solid #ddbdfc;
+  }
+
+	.email-ol {
+		float: right;
+		position: relative;
+		top: -52px;
+	}
+
+  .grid-button {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-row-gap: 20px;
+  }
+
+  .content {
+    height: 48px;
+    min-width: 148px;
+    text-align: center;
+    line-height: initial;
+    background: linear-gradient(90deg, #ecd9ff 0%, #ddbdfc 100%);
+  }
+
+  .content-text {
+    background: linear-gradient(90deg, #8327df 0%, #3c1267 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+  }
+  
+  .right {
+    width: 0;
+    height: 0;
+    background-color: #ddbdfc;
+    border-right: 12px solid rgb(255, 255, 255);
+    border-left: 24px solid transparent;
+    border-top: 24px solid transparent;
+    border-bottom: 24px solid transparent;
+  }
+
+  .left {
+    width: 0;
+    height: 0;
+    background-color: #ecd9ff;
+    border-right: 24px solid transparent;
+    border-left: 12px solid rgb(255, 255, 255);
+    border-top: 24px solid transparent;
+    border-bottom: 24px solid transparent;
+  }
+
+	ul {
+		padding: 20px;
+		list-style: disc;
+	}
+
+	li {
+		margin-bottom: 0.5rem;
+	}
+</style>
