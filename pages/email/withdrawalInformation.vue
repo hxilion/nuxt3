ec0d9803ba7f23b399b767aa36b4a06e556f4374
Lion <lion@luckylab.page>
2024-04-15 13:13:47 +0800

    feature: 新增提款資訊頁面/手機版

---
+++ pages/email/withdrawalInformation.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="email photo px-10 pb-10 text-color-1 font-medium" style="background-color: #902BF5;width:100%;min-width: 470px;font-family:'M PLUS 2'">
+    <div>
+      <img src="https://static4.smr.vc/fce0e306bd7aea9abe7105-desktop.png" alt="">
+    </div>
+    <div class="email-content" style="max-width: 640px; min-width: 390px;">
+      <img src="/assets/img/email/withdrawal-info.png" alt="">
+      <div class="text-sm text-left text-start p-10" style="background-color: #fff;">
+        <div class="mb-10">
+          %user_name% 様
+        </div>
+        <div class="mb-10">
+          いつもPazuruをご利用いただき、誠にありがとうございます。
+        </div>
+        <div>
+          この度、%User_Name%様の退会手続きが完了いたしましたので、ご連絡申し上げます。
+        </div>
+        <div class="mb-10">
+          退会手続きの完了に伴い、%User_Name%様のアカウントは削除され、ログインやサービスの利用ができなくなります。
+        </div>
+        <div class="mb-10" style="color: #f70d1a;">
+          また、%User_Name%様がこれまでに作成されたコンテンツやデータもすべて削除されますので、予めご了承ください。
+        </div>
+        <div class="divide-x mb-14"></div>
+        <div>
+          Pazuruは、%User_Name%様のご意見やご要望を参考に、より良いサービスを提供できるよう努めてまいります。
+        </div>
+        <div class="mb-10">
+          今後ともPazuruをよろしくお願いいたします。
+        </div>
+        <div>
+          %User_Name%様にとって、素晴らしい未来が訪れることを心から願っております。
+        </div>
+        <div class="mb-10">
+          またのご利用をお待ちしております。
+        </div>
+        <div class="mb-14">
+          Pazuru運営チーム
+        </div>
+        <div class="text-color-2 text-xs">
+          ※このメールは自動送信のため、返信できません。
+        </div>
+        <div class="text-color-2 text-xs">
+          ※このメールに心当たりのない場合は、お手数ですがこのメールを削除してください。
+        </div>
+      </div>
+      <div class="email-conect text-sm text-left text-start px-10 pt-5 pb-5 font-medium rounded-b-md" style="background-color: #F4EAFE;max-width: 640px; min-width: 390px;">
+        <div class="email-ol">
+          <img src="/assets/img/email/email-ol.png" alt="">
+        </div>
+        <div class="text-color-1 text-xs font-bold mb-2.5">
+          Pazuru運営事務局
+        </div>
+        <div class="text-color-1 text-xs mb-2.5 whitespace-nowrap">
+          メールアドレス：info@pazuru.com
+        </div>
+        <div class="text-color-1 text-xs">
+          ※営業時間：
+        </div>
+      </div>
+      <div class="email-about text-sm font-medium px-10 pt-5 pb-5" style="max-width: 640px; min-width: 390px;">
+        <div class="container-flex" style="width: 100%;">
+          <div class="image-wrapper">
+            <img src="/assets/img/email/facebook.png" alt="">
+          </div>
+          <div class="image-wrapper">
+            <img src="/assets/img/email/instagram.png" alt="">
+          </div>
+          <div class="image-wrapper">
+            <img src="/assets/img/email/twitter.png" alt="">
+          </div>
+          <div class="image-wrapper">
+            <img src="/assets/img/email/youtube.png" alt="">
+          </div>
+          <div class="image-wrapper">
+            <img src="/assets/img/email/tiktok.png" alt="">
+          </div>
+        </div>
+        <div class="my-10">
+          <img src="/assets/img/email/pazuru-logo-white.png" alt="">
+        </div>
+        <div class="text-xs" style="color: #f4eafe;">
+          <a class="divide-y" href="/">プライバシーポリシー</a>
+          <a class="divide-y" href="/">利用規約</a>
+          <a class="divide-y" href="/">よくある質問</a>
+          <a href="/">カスタマーサポート</a>
+        </div>
+        <div class="text-xs mt-10" style="color: #fff;">
+          ©Pazuru 2024 | 全著作権所有
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .photo {
+    text-align: -webkit-center;
+  }
+
+  .divide-x {
+    border-bottom: 1px solid #CC9DFA;
+  }
+
+  .divide-y {
+    border-right: 1px solid #F4EAFE;
+    margin-right: 10px;
+    padding-right: 10px;
+  }
+
+  .text-color-1 {
+    color: #131416;
+  }
+  .text-color-2 {
+    color: #727478;
+  }
+
+  .container-flex {
+    display: flex;
+    justify-content: center; /* 將圖片水平置中 */
+    align-items: center; /* 將圖片垂直置中 */
+  }
+
+  .image-wrapper {
+    margin: 0 10px; /* 可根據需要調整圖片之間的間距 */
+  }
+
+  .image-wrapper img {
+    max-width: 100%; /* 讓圖片保持其原始大小比例 */
+    height: auto;
+  }
+
+  .mail-verify {
+    width: 209px;
+    height: 52px;
+    background: linear-gradient(#a655f7, #902BF5);
+    border: 2px solid #ddbdfc;
+  }
+
+  .email-ol {
+    float: right;
+    position: relative;
+    top: -52px;
+  }
+
+  /* @media (max-width: 1024px) {
+    .email {
+      padding: 0 40px 40px 40px;
+    }
+    .email-content {
+      max-width: 640px; min-width: 390px;
+    }
+    .email-conect {
+      padding: 20px 40px 20px 40px;
+      max-width: 640px; min-width: 390px;
+    }
+    .email-about {
+      max-width: 640px;
+      min-width: 390px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .email {
+      padding: 0 40px 40px 40px;
+    }
+    .email-content {
+      max-width: 640px; min-width: 390px;
+    }
+    .email-conect {
+      padding: 20px 40px 20px 40px;
+      max-width: 640px; min-width: 390px;
+    }
+    .email-about {
+      max-width: 640px;
+      min-width: 390px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .email-conect {
+      padding: 20px;
+    }
+  } */
+</style>
