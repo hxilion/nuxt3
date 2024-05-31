52b03dfc43e22e1962f76cc0335b8dde841c86d7
Dean <dean@luckylab.page>
2024-05-31 11:30:10 +0800

    feature: 可以產出靜態頁面

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
+  ssr: true,
   typescript: {
     typeCheck: true
+  },
+  nitro: {
+    preset: 'browser'
   },
   build: {
     transpile: ['@headlessui/vue']
