331b2bbf508c7a884278fe1931bbf350f6453078
Dean <dean@luckylab.page>
2024-05-31 09:49:29 +0800

    feature: 部署gitlab

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+    nuxtIcon: {
+      size: '24px',
+      class: 'icon-class',
+      aliases: {
+      },
+      iconifyApiOptions: {
+        url: '',
+        publicApiFallback: true,
+      },
+    },
+  });
