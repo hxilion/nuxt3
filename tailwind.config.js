712a6bfb2b9beaeff3b10740fe2f60fc49512f9a
Lion <lion@luckylab.page>
2024-05-31 10:35:57 +0800

    update: 調整縮排

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          primary: '#00FF50',
-          secondary: '#1E1E1E',
-          grayD2: '#D2D2D2',
-          gray96: '#969696',
-          grayF8: '#F8F8F8',
-          redDC: '#DC0000',
-          primaryVariant: '#80FFA7'
-        },
-        boxShadow: {
-          '2': '0px 8px 24px 0px #1E1E1E1A'
-        },
-        boxShadow: {
-          'ctz1': '0px 8px 24px 0px rgba(201, 210, 210)',
-        }
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00FF50',
+        secondary: '#1E1E1E',
+        grayD2: '#D2D2D2',
+        gray96: '#969696',
+        grayF8: '#F8F8F8',
+        redDC: '#DC0000',
+        primaryVariant: '#80FFA7'
       },
+      boxShadow: {
+        '2': '0px 8px 24px 0px #1E1E1E1A'
+      },
+      boxShadow: {
+        'ctz1': '0px 8px 24px 0px rgba(201, 210, 210)',
+      }
     },
-    variants: {},
-    plugins: [],
-  }
+  },
+  variants: {},
+  plugins: [],
+}
